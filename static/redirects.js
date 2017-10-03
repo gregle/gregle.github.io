7fbc3109a64356ffadb4db72ea97d8687e8994af
gregle <justin@jcrastelli.com>
2017-10-03 15:19:38 -0700

    update

---
+++ static/redirects.js
@@ -0,0 +1,14 @@
+var _redirects = [
+  {
+    'path': '/tunamelt',
+    'redirect': 'https://i.imgur.com/PEL8meh.jpg'
+  },
+  {
+    'path': '/humble',
+    'redirect': 'https://www.humblebundle.com/gems-bundle-2?partner=Lowco2525'
+  },
+  {
+    'path': '/humblestore',
+    'redirect': 'https://www.humblebundle.com/store?partner=Lowco2525'
+  }
+]
