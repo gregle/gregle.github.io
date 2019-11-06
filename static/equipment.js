8adaa9b0f12d1da3f11e841d0e5dc919fe27e1ac
Gregle <jusman98@gmail.com>
2019-11-05 22:25:57 -0800

    update

---
+++ static/equipment.js
@@ -0,0 +1,252 @@
+var _equipment = {
+	"affiliateTag": "?tag=lowco2525",
+	"gaming": [
+		{
+			"type": "Processor",
+			"list": [
+				{
+					"title": "Intel Core i7 6700k",
+					"link": "https://www.amazon.com/dp/B012M8LXQW/"
+				}
+			]
+		},
+		{
+			"type": "CPU Cooler",
+			"list": [
+				{
+					"title": "NZXT Kraken X61",
+					"link": "https://www.amazon.com/dp/B00L0YLJJG/"
+				}
+			]
+		},
+		{
+			"type": "Motherboard",
+			"list": [
+				{
+					"title": "Asus Z170-PRO",
+					"link": "https://www.amazon.com/dp/B016A44XHK/"
+				}
+			]
+		},
+		{
+			"type": "RAM",
+			"list": [
+				{
+					"title": "16GB Corsair Dominator DDR4",
+					"link": "https://www.amazon.com/dp/B018GK2G9S/"
+				}
+			]
+		},
+		{
+			"type": "Graphics Card",
+			"list": [
+				{
+					"title": "GEFORCE® GTX 1080 Ti",
+					"link": "https://www.amazon.com/dp/B06XH2P8DD/"
+				}
+			]
+		},
+		{
+			"type": "Hard Drive",
+			"list": [
+				{
+					"title": "Western Digital 2TB",
+					"link": "https://www.amazon.com/dp/B00FJRS628/"
+				},
+				{
+					"title": "Samsung M.2 512GB",
+					"link": "https://www.amazon.com/dp/B01639694M/"
+				}
+			]
+		},
+		{
+			"type": "Power Supply",
+			"list": [
+				{
+					"title": "EVGA 80+ Platinum 850W",
+					"link": "https://www.amazon.com/dp/B010HWDOH6/"
+				}
+			]
+		},
+		{
+			"type": "Case",
+			"list": [
+				{
+					"title": "NZXT H440",
+					"link": "https://www.amazon.com/dp/B017C11594/"
+				}
+			]
+		}
+	],
+	"streaming": [
+		{
+			"type": "Processor",
+			"list": [
+				{
+					"title": "AMD Ryzen 7 1700 Processor",
+					"link": "https://www.amazon.com/dp/B06WP5YCX6/"
+				}
+			]
+		},
+		{
+			"type": "Motherboard",
+			"list": [
+				{
+					"title": "ASROCK AB350 PRO4",
+					"link": "https://www.amazon.com/dp/B06XKSM4BW/"
+				}
+			]
+		},
+		{
+			"type": "RAM",
+			"list": [
+				{
+					"title": "Corsair Vengeance LPX 16GB DDR4",
+					"link": "https://www.amazon.com/dp/B017NW5NZY/"
+				}
+			]
+		},
+		{
+			"type": "Graphics Card",
+			"list": [
+				{
+					"title": "NVIDIA GeForce GTX 780 (Zotac)",
+					"link": "https://www.amazon.com/dp/B00CWM95XC"
+				}
+			]
+		},
+		{
+			"type": "Hard Drive",
+			"list": [
+				{
+					"title": "Western Digital 2TB",
+					"link": "https://www.amazon.com/dp/B00FJRS628/"
+				},
+				{
+					"title": "Samsung 256GB SSD",
+					"link": "https://www.amazon.com/dp/B01LCHCTJE/"
+				}
+			]
+		},
+		{
+			"type": "Power Supply",
+			"list": [
+				{
+					"title": "Silverstone Strider Plus 850W",
+					"link": "https://www.amazon.com/dp/B00NEVOBHU/"
+				}
+			]
+		},
+		{
+			"type": "Case",
+			"list": [
+				{
+					"title": "Fractal Design Define R4",
+					"link": "https://www.amazon.com/dp/B008HD3CTI/"
+				}
+			]
+		}
+	],
+	"peripherals": [
+		{
+			"type": "Monitors",
+			"list": [
+				{
+					"title": "Samsung S27D590P",
+					"link": "https://www.amazon.com/dp/B00IEZGW8C/"
+				},
+				{
+					"title": "ASUS VS248H-P",
+					"link": "https://www.amazon.com/dp/B0058UUR6E/"
+				},
+				{
+					"title": "Dell S2340M",
+					"link": "https://www.amazon.com/dp/B009H0XQRS/"
+				}
+			]
+		},
+		{
+			"type": "Webcam",
+			"list": [
+				{
+					"title": "Logitech C920",
+					"link": "https://www.amazon.com/dp/B006JH8T3S/"
+				}
+			]
+		},
+		{
+			"type": "Microphone",
+			"list": [
+				{
+					"title": "Shure SM7B",
+					"link": "https://www.amazon.com/dp/B0002E4Z8M/"
+				}
+			]
+		},
+		{
+			"type": "Headset",
+			"list": [
+				{
+					"title": "Razer Kraken Pro V2 Quartz",
+					"link": "https://www.amazon.com/dp/B077XGF3WB/"
+				}
+			]
+		},
+		{
+			"type": "Keyboard",
+			"list": [
+				{
+					"title": "Razer Deathstalker",
+					"link": "https://www.amazon.com/dp/B009SJR28W/"
+				}
+			]
+		}
+	],
+	"peripherals2": [
+		{
+			"type": "Mouse",
+			"list": [
+				{
+					"title": "Razer Naga Trinity",
+					"link": "https://www.amazon.com/dp/B0788MCRGC/"
+				}
+			]
+		},
+		{
+			"type": "Mousepad",
+			"list": [
+				{
+					"title": "Razer Golitathus",
+					"link": "https://www.amazon.com/dp/B002R0DX0A/"
+				}
+			]
+		},
+		{
+			"type": "Capture Card",
+			"list": [
+				{
+					"title": "Elgato GameCapture HD60 Pro",
+					"link": "https://www.amazon.com/dp/B014MQIVPS/"
+				}
+			]
+		},
+		{
+			"type": "Chair",
+			"list": [
+				{
+					"title": "Herman Miller Embody",
+					"link": "https://www.amazon.com/dp/B01DGM7ZII"
+				}
+			]
+		},
+		{
+			"type": "Internet Provider",
+			"list": [
+				{
+					"title": "Potato",
+					"link": "https://www.amazon.com/dp/B000NSKB7Q/"
+				}
+			]
+		}
+	]
+}
---
+++ static/redirects.js
@@ -0,0 +1,222 @@
+var _redirects = [
+  {
+    'path': '/SCOgineer',
+    'redirect': 'https://cdn.discordapp.com/attachments/95547477029040128/580941559609163786/image0.jpg'
+  },
+  {
+    'path': '/WesPride',
+    'redirect': 'https://cdn.discordapp.com/attachments/95547477029040128/580935455575506974/60820025_10156052404251856_857496371039567872_n.png'
+  },
+  {
+    'path': '/sekirodeaths',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLSeR6uHMEZiDax6cxRJaHzjFyX9B8hhZ_EZ8ZGfzLQoICuHLgg/viewform?usp=sf_link'
+  },
+  {
+    'path': '/sekiro',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLSerSDSheMhzpoq2Rm5lfd4vzvFPUC8kZJNsNJxZBrDUHoJb1g/viewform'
+  },
+  {
+    'path': '/charity',
+    'redirect': 'https://tiltify.com/@lowco/lowco-streams-for-dreams-campaign'
+  },
+  {
+    'path': '/viruscontest',
+    'redirect': 'https://goo.gl/forms/ufcy4Fq3spOWrATi2'
+  },
+  {
+    'path': '/secretsanta',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLSf-wfUWtkJ2mTdSy-UC6XGp916XqZcOP5CM6mQzvrObJP1uDQ/viewform?usp=sf_link'
+  },
+  {
+    'path': '/veterans',
+    'redirect': 'https://tiltify.com/@lowco/lowco-veterans-day-2018-campaign'
+  },
+  {
+    'path': '/twopoint',
+    'redirect': 'https://gleam.io/5HojZ/lc-tph-key-giveaway'
+  },
+  {
+    'path': '/whf',
+    'redirect': 'https://gleam.io/zbucu/lowco-we-happy-few-pc-giveaway'
+  },
+  {
+    'path': '/q4tc',
+    'redirect': 'https://docs.google.com/document/d/1wo-4pBU0eB9QVCLvWLuZ6cJG6wpxxYjE_RLbZ-3Jv8c/edit?usp=sharing'
+  },
+  {
+    'path': '/fashionframe',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLSeyCp9dOlGS7O7JIktm99gg-SNiC79YEoePFtfm2ybLy22oMQ/viewform?usp=sf_link'
+  },
+  {
+    'path': '/warframe',
+    'redirect': 'https://www.warframe.com/signup?referrerId=53f87c1c384632ba51a60e95'
+  },
+  {
+    'path': '/merch',
+    'redirect': 'https://www.designbyhumans.com/shop/Lowco/'
+  },
+  {
+    'path': '/serverrules',
+    'redirect': 'https://docs.google.com/document/d/1NKDmbvyOQpXM6QahpCk8MCEUbHclshoybb0ZNsaBJRU/edit?usp=sharing'
+  },
+  {
+    'path': '/feedback',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLScdPcMVR8x5XCSgkt7ucso66672og4GPepCZ48ASU6NNoBWMA/viewform'
+  },
+  {
+    'path': '/humblestore',
+    'redirect': 'https://www.humblebundle.com/store?partner=lowco'
+  },
+  {
+    'path': '/DBH',
+    'redirect': '//dbh.7eer.net/c/258766/301861/4686?subId1=Lowco&u=https%3A%2F%2Fwww.designbyhumans.com%2F'
+  },
+  {
+    'path': '/ShadowsOfKyloRen',
+    'redirect': '//dbh.7eer.net/c/258766/301861/4686?subId1=Lowco&u=https%3A%2F%2Fwww.designbyhumans.com%2Fshop%2Ft-shirt%2Fmen%2Fshadows-of-kylo-ren%2F159788%2F'
+  },
+  {
+    'path': '/Vault111',
+    'redirect': '//dbh.7eer.net/c/258766/301861/4686?subId1=Lowco&u=https%3A%2F%2Fwww.designbyhumans.com%2Fshop%2Ft-shirt%2Fmen%2Fvault-111%2F143050%2F'
+  },
+  {
+    'path': '/teamfood',
+    'redirect': '//dbh.7eer.net/c/258766/301861/4686?subId1=Lowco&u=https%3A%2F%2Fwww.designbyhumans.com%2Fshop%2Ft-shirt%2Fmen%2Fteam-food%2F278986%2F'
+  },
+  {
+    'path': '/stjudeinfo',
+    'redirect': 'https://docs.google.com/document/d/1hMOB6Vx59N2e4pKIrHJdRxBHDZ5UcNJSx_KrABKiKOw/edit?usp=sharing'
+  },
+  {
+    'path': '/trolldiscord',
+    'redirect': 'https://www.youtube.com/watch?v=-aJDHGnIVaQ'
+  },
+  {
+    'path': '/unban',
+    'redirect': 'https://docs.google.com/forms/d/e/1FAIpQLSfraN1-8tLUIIP57GLd0LErI9A2jhZLnJ3tc0RGdop_diEuxg/viewform'
+  },
+  {
+    'path': '/triggered',
+    'redirect': 'https://cdn.discordapp.com/attachments/95491878400557056/188321834935123972/triggered.gif'
+  },
+  {
+    'path': '/scary',
+    'redirect': 'https://www.youtube.com/user/Lowco2525/playlists?sort=dd&view=50&shelf_id=14'
+  },
+  {
+    'path': '/razer',
+    'redirect': 'http://www.anrdoezrs.net/links/8837851/type/dlg/https://www.razer.com/'
+  },
+  {
+    'path': '/bingo',
+    'redirect': 'http://jcrastelli.com/projects/bingo/#/'
+  },
+  {
+    'path': '/amazonca',
+    'redirect': 'https://www.amazon.ca/?_encoding=UTF8&tag=lowco252502-20'
+  },
+  {
+    'path': '/amazonde',
+    'redirect': 'https://www.amazon.de/ref=as_li_ss_tl?_encoding=UTF8&camp=1638&creative=19454&linkCode=ur2&site-redirect=de&tag=lowco252507-21'
+  },
+  {
+    'path': '/amazonuk',
+    'redirect': 'https://www.amazon.co.uk/ref=as_li_ss_tl?_encoding=UTF8&camp=1634&creative=19450&linkCode=ur2&tag=lowco2525-21'
+  },
+  {
+    'path': '/amazonus',
+    'redirect': 'https://www.amazon.com/ref=as_li_ss_tl?ie=UTF8&linkCode=sl2&tag=lowco2525-20&linkId=c32983976b19db74162534f6d63fc6ed'
+  },
+  {
+    'path': '/amazones',
+    'redirect': 'https://www.amazon.es/?_encoding=UTF8&tag=lowco25250e-21'
+  },
+  {
+    'path': '/tunamelt',
+    'redirect': 'https://i.imgur.com/PEL8meh.jpg'
+  },
+  {
+    'path': '/humble',
+    'redirect': 'https://www.humblebundle.com/?partner=Lowco'
+  },
+  {
+    'path': '/harvey',
+    'redirect': 'https://tiltify.com/events/lowco-hurricane-harvey-relief-campaign  '
+  },
+  {
+    'path': '/monthly-giveaway',
+    'redirect': 'https://gleam.io/MxPmQ/september-monthly-community-giveaway-100-steam-giftcard'
+  },
+  {
+    'path': '/subathon',
+    'redirect': 'https://docs.google.com/document/d/1oJJJJGEz9YSD1XuPS1G_Fh0_g0w1GtRwNNgdoB19dCw/edit?usp=sharing'
+  },
+  {
+    'path': '/950',
+    'redirect': 'https://gleam.io/SHNBW/500-greenmangaming-giveaway-950-subpoints-celebration'
+  },
+  {
+    'path': '/SOS',
+    'redirect': 'https://secure.xsolla.com/api/network/?utm_source=Z3serDhV&utm_campaign=outpost&click_id=none'
+  },
+  {
+    'path': '/giveaway',
+    'redirect': ' https://gleam.io/v4lBq/metro-exodus-giveaway'
+  },
+  {
+    'path': '/pubg',
+    'redirect': 'https://secure.xsolla.com/api/network/?utm_source=Z3serDhV&utm_campaign=5a9e398952d9f&click_id=none'
+  },
+  {
+    'path': '/stjude',
+    'redirect': 'https://tiltify.com/@lowco/lowco-st-jude-2019'
+  },
+  {
+    'path': '/stjudedxracer',
+    'redirect': 'https://gleam.io/yQEbw/lowco-community-st-jude-dxracer-giveaway'
+  },
+  {
+    'path': '/stjudeconsole',
+    'redirect': 'https://gleam.io/HIdhj/lowco-community-console-giveaway'
+  },
+  {
+    'path': '/stjudedonate',
+    'redirect': 'https://tiltify.com/@lowco/lowco-st-jude-2018/donate'
+  },
+  {
+    'path': '/amazon',
+    'redirect': 'https://lowco.tv/community/blog/2014/05/amazon-referral-links'
+  },
+  {
+    'path': '/candy',
+    'redirect': 'https://lowco.tv/community/blog/2014/05/getting-to-know-candy-channel-currency'
+  },
+  {
+    'path': '/games',
+    'redirect': 'https://lowco.tv/community/blog/2014/05/lowcos-gaming-repertoire'
+  },
+  {
+    'path': '/ship',
+    'redirect': 'https://docs.google.com/document/d/1d0fUAabPUHzIRpo_ONOC4jrYRe6I9dfiRunEcoy608E/edit?usp=sharing'
+  },
+  {
+    'path': '/shirt',
+    'redirect': 'https://www.designbyhumans.com/shop/t-shirt/men/team-catalyst-st-jude-2018-tee/904780/'
+  },
+  {
+    'path': '/humblefav',
+    'redirect': 'https://www.humblebundle.com/store/promo/lowco-favorites?partner=lowco'
+  },
+  {
+    'path': '/razer-giveaway',
+    'redirect': 'https://gleam.io/TatS9/lowco-razer-quartz-giveaway'
+  },
+  {
+    'path': '/Jurassic',
+    'redirect': 'https://gleam.io/2uTpn/jurassic-world-evolution-giveaway'
+  },
+  {
+    'path': '/displate',
+    'redirect': 'https://displate.com/lowco/displates?art=5c351488037e5'
+  }
+]
